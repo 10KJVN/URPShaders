5ace778b43217d55715a358b7d18a2908a82cb5e
10KJVN <90614291+10KJVN@users.noreply.github.com>
2024-09-21 22:28:10 +0200

    Bug & Warning fixes resolved, works??

--- Assets/Scripts/TerrainRenderer.cs
+++ Assets/Scripts/TerrainRenderer.cs
@@ -12,8 +12,10 @@ public class TerrainRenderer : MonoBehaviour
         mainCamera = Camera.main;
 
         // Set up the output texture
-        outputTexture = new RenderTexture(Screen.width, Screen.height, 24);
-        outputTexture.enableRandomWrite = true;
+        outputTexture = new RenderTexture(Screen.width, Screen.height, 24)
+        {
+            enableRandomWrite = true
+        };
         outputTexture.Create();
     }
 
@@ -25,11 +27,18 @@ public class TerrainRenderer : MonoBehaviour
         computeShader.SetFloat("_AspectRatio", (float)Screen.width / Screen.height);
         computeShader.SetVector("_CameraPosition", mainCamera.transform.position);
         computeShader.SetVector("_CameraForward", mainCamera.transform.forward);
+        computeShader.SetVector("_CameraRight", mainCamera.transform.right);
+        computeShader.SetVector("_CameraUp", mainCamera.transform.up);
+        
+        // Set the output texture for both _Result and Result
+        int kernelHandle = computeShader.FindKernel("CSMain");
+        computeShader.SetTexture(kernelHandle, "_Result", outputTexture);
+        computeShader.SetTexture(kernelHandle, "Result", outputTexture);
         
         // Dispatch the compute shader (number of thread groups based on screen size)
         int threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
         int threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
-        computeShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
+        computeShader.Dispatch(kernelHandle, threadGroupsX, threadGroupsY, 1);
         
         // Assign the output texture to a material for display
         GetComponent<Renderer>().material.mainTexture = outputTexture;
