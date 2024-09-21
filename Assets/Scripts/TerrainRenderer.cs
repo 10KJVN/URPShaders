f1f203761882e9774f684c4367943ea7cc8d002d
10KJVN <90614291+10KJVN@users.noreply.github.com>
2024-09-21 21:50:24 +0200

    Added the update method

--- Assets/Scripts/TerrainRenderer.cs
+++ Assets/Scripts/TerrainRenderer.cs
@@ -20,6 +20,18 @@ public class TerrainRenderer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Set camera parameters
+        computeShader.SetFloat("_FieldOfView", mainCamera.fieldOfView);
+        computeShader.SetFloat("_AspectRatio", (float)Screen.width / Screen.height);
+        computeShader.SetVector("_CameraPosition", mainCamera.transform.position);
+        computeShader.SetVector("_CameraForward", mainCamera.transform.forward);
         
+        // Dispatch the compute shader (number of thread groups based on screen size)
+        int threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
+        int threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
+        computeShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
+        
+        // Assign the output texture to a material for display
+        GetComponent<Renderer>().material.mainTexture = outputTexture;
     }
 }
